751d66826dce4c5ae8004d23c69644ff897e221d
Harshan <Harshan@Harshan-PC>
2018-06-28 04:48:41 +0530

    First commit of Spring boot Addition rest service

---
+++ AdditionRestService/src/main/java/com/logantechsource/addition/AdditionRestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.logantechsource.addition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionRestServiceApplication.class, args);
+	}
+}
---
+++ AdditionRestService/src/test/java/com/logantechsource/addition/AdditionRestServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.logantechsource.addition;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdditionRestServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
