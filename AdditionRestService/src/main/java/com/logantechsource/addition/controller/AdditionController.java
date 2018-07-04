e0b1d1717ac932660a6ced373b94519ec0aba0df
587518 <587518@vagrant-VirtualBox>
2018-07-04 06:49:17 -0800

    Added subtraction method

--- AdditionRestService/src/main/java/com/logantechsource/addition/controller/AdditionController.java
+++ AdditionRestService/src/main/java/com/logantechsource/addition/controller/AdditionController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdditionController {
 
 	/**
-	 * This GET method will add numbers which are passed through request parameter. 
+	 * This GET method will add the numbers which are passed through request parameter. 
 	 * Since we have used Varargs (...) parameter we can pass more numbers with comma separated values as argument.
 	 * 
 	 * @param numbersToAdd is the RequestParam variable and it is alias of "numbers". 
@@ -38,6 +38,33 @@ public class AdditionController {
 				sumTotal += Integer.parseInt(number);
 			}
 			responseMessage = "The given "+ numbersToAdd.length + " number(s) sum value is " +sumTotal;
+		}
+		catch(NumberFormatException numberFormatException)
+		{
+			responseMessage = "Please provide valide data!";
+		}
+		return responseMessage;
+	}
+	
+	/**
+	 * This GET method will subtract the numbers which are passed through request parameter. 
+	 * Since we have used Varargs (...) parameter we can pass more numbers with comma separated values as argument.
+	 *
+	 * @param numbersToSubtract the numbers to subtract
+	 * @return the subtraction of the given numbers
+	 */
+	@GetMapping(value="/subtract")
+	public String subtractNumbers(@RequestParam("numbers") String... numbersToSubtract)
+	{
+		int subtractTotal = 0;
+		String responseMessage = null;
+		try
+		{
+			for(String number: numbersToSubtract)
+			{
+				subtractTotal -= Integer.parseInt(number);
+			}
+			responseMessage = "The given "+ numbersToSubtract.length + " number(s) subtraction value is " +subtractTotal;
 		}
 		catch(NumberFormatException numberFormatException)
 		{
